b5e4bfdcc5ceec0932aced40f5ea2a8484b59f1d
eya <bensaid.eya@esprit.tn>
2022-11-29 21:47:14 +0100

    second commit main.cpp

---
+++ mainwindow.h
@@ -0,0 +1,57 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "client.h"
+#include "smtp.h"
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+
+private slots:
+    void on_B_ajouter_clicked();
+
+    void on_pb_supp_clicked();
+
+    void on_pb_modif_clicked();
+
+
+
+    void on_B_tri_clicked();
+
+
+
+    void on_B_rech_clicked();
+
+    void on_B_pdf_clicked();
+    void sendMail();
+    void mailSent(QString );
+
+
+
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pb_stat_clicked();
+
+private:
+    Ui::MainWindow *ui;
+   Client C;
+     QStringList files;
+       QTcpSocket *socket;
+
+
+};
+#endif // MAINWINDOW_H
